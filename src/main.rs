68e7ea7fe1b768f7607a0f7d17996e207e7db121
yopocho <nielsio@hotmail.com>
2026-04-12 23:13:38 +0200

    Simple hello world for figuring out rust build systems

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+#![no_std]
+#![no_main]
+
+use cortex_m_rt::entry;
+use panic_halt as _;
+
+#[entry]
+fn main() -> ! {
+    loop {
+        let mut _i: usize = 0;
+        for _ in 0..10_000 {
+            cortex_m::asm::nop();
+            _i += 1;
+        }
+    }
 }
